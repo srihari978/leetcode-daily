238785d8f74635cebf28c6167987eb3fadbc251d
Srihari <122339787+srihari978@users.noreply.github.com>
2026-08-28 20:15:01 +0530

    Time: 7 ms (84.98%) | Memory: 19.5 MB (85.65%) - LeetSync

---
+++ 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+       int n=nums.size();
+        int sum=0;
+        // sort(nums.begin(),nums.end());
+        for(int i=1;i<n;i++){
+            if(nums[i-1]>=nums[i]){
+                int x=nums[i-1]-nums[i]+1;
+                sum+=x;
+                nums[i]=x+nums[i];
+            }
+        }
+        return sum;  
+    }
+};
